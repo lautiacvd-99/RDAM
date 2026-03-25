9191bbc2f962531221320c6f9004f439db8f3955
lautiacvd-99 <lauti.acvd@gmail.com>
2026-03-24 21:50:41 -0300

    coreccion de cors otp

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(helmet({
 
 // ✅ CORS 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: ["http://localhost:5173", "http://localhost"],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
